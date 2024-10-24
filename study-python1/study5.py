add790a631fe6f17fddd21d18a1373eb7213ede4
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-24 18:12:32 -0300

    update-studies2

---
+++ study-python1/study5.py
@@ -0,0 +1,11 @@
+#Operadores em Python
+
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 30 / 3
+potencia = 7 ** 2
+
+print("soma", soma)
+print("multiplicacao", multiplicacao)
+print("divisão", divisao)
+print("potência", potencia)
---
+++ study-python1/study6.py
@@ -0,0 +1,9 @@
+#Condicionais em Python
+
+valor1 = 10
+valor2 = 50
+
+if valor1 > valor2:
+    print(valor1, "é maior que", valor2)
+else:
+    print(valor2, "é maior que", valor1)    
