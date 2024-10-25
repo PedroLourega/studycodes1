79844b3832dc2e44d864ae7840f44c339cdb1dba
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-25 10:06:49 -0300

    update_studies5

---
+++ study-python1/study17.py
@@ -0,0 +1,25 @@
+import os
+#os.getcws() - Obtém o diretório de trabalho atual.
+#os.listdir(path) - Lista os arquivos e diretórios em um caminho específico.
+#os.path.join(path, *paths) - Combina partes de caminhos para formar um novo caminho.
+#os.path.exists(path) - Verifica se um caminho existe.
+#os.mkdir(path) - Cria um novo diretório.
+#os.remove(path) - Remove um arquivo.
+#os.rename(src, dst) - Renomeia um arquivo ou diretório.
+#os.environ - Deicionário com variáveis de ambiente do sistema.
+#os.system(command) - Executa um comando no sistema.
+#os.chdir - Altera o diretório de trabalho atual.
+
+mensagens = []
+
+nome= input("Nome: ")
+
+while True:
+
+    #limpando terminal
+
+    os.system('cls')
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
