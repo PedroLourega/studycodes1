8ba32b16bbb4edc797baa4347688513962c0efa5
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-24 22:24:39 -0300

    Revert "update_studies3"
    This reverts commit ae127208bc614426497520854043738d32e0754c.

--- study-python1/study10.py
+++
@@ -1,3 +0,0 @@
-numeros = [1, 2, 3]
-strings = ["Joao", "Maria", "Bruxa"]
-decimais = [10.8, 15.2, 33.33]
--- study-python1/study9.py
+++
@@ -1,8 +0,0 @@
-#Listas em Python
-
-lista_numeros = [1, 2, 3]
-
-#Os arrays começam em 0
-print(lista_numeros[0])
-print(lista_numeros[1])
-print(lista_numeros[2])
